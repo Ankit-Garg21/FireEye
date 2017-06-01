6d3d1f400fdd7d6260a9918e81f187c0b24b9d74
Ankit <ankit00239@gmail.com>
2017-06-01 05:47:50 +0530

    Added code for embedding twitter timeline

--- public/scripts/Mockup.js
+++ public/scripts/Mockup.js
@@ -1,16 +1,35 @@
 function Mockup() {};
 
 Mockup.pageId = "pgMockup";
-Mockup.config = {};
+Mockup.config = {
+    TIMELINES: [
+        "ankit00239",
+        "virendersehwag"
+    ]
+};
+
+//<a class="twitter-timeline" href="https://twitter.com/ankit00239">Tweets by ankit00239</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 Mockup.init = function() {
     $( ".page" ).hide();
     $( "#" + Mockup.pageId ).show();
 
     var config = Mockup.config;
-    Mockup.bind();
+    Mockup.loadTimelines( config )
 };
 
 Mockup.bind = function() {
     
 };
+
+Mockup.loadTimelines = function( config ) {
+    config.TIMELINES && config.TIMELINES.forEach( function( timeline ) {
+        var html = "";
+        html += '<a class="twitter-timeline" href="https://twitter.com/' + timeline +'">';
+        html += 'Tweets by ' + timeline;
+        html += '</a>';
+        html += '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+
+        $( "#" + Mockup.pageId + " .container" ).append( html );
+    });
+}
