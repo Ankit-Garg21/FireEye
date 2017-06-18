b40332992ae7f4fbf0a579e2e210a575b9960552
Ankit <ankit00239@gmail.com>
2017-06-18 09:48:22 +0530

    Added create tweet modal.

--- public/scripts/Mockup.js
+++ public/scripts/Mockup.js
@@ -5,6 +5,7 @@ Mockup.pageId = "pgMockup";
 Mockup.init = function() {
     $( ".page" ).hide();
     $( "#" + Mockup.pageId ).show();
+    Mockup.config = {};
 
     $( "#" + Mockup.pageId + " .container .resp-tabs-list" ).empty();
     $( "#" + Mockup.pageId + " .container .resp-tabs-container" ).empty();
@@ -13,9 +14,44 @@ Mockup.init = function() {
         url: 'common/data.json',
         dataType: 'json',
         success: function( config ) {
-            console.log(config);
+            Mockup.config = config;
             Mockup.loadTimelines( config );
         }
+    });
+
+    Mockup.bindEvents();
+};
+
+Mockup.bindEvents = function() {
+    var overlay = '<div class="modal-overlay modal-close"></div>';
+    $( "a[ data-modal-id ]" ).unbind( "click" );
+    $( "a[ data-modal-id ]" ).bind( "click", function( e ) {
+        e.preventDefault();
+        $( "body" ).append( overlay );
+        $( ".modal-overlay" ).fadeTo( 500, 0.7 );
+		var modalBox = $( this ).attr( "data-modal-id" );
+		$( "#" + modalBox ).fadeIn( $( this ).data() );
+    });
+
+    $( ".modal-close, .modal-overlay" ).click( function() {
+        $( ".modal-box, .modal-overlay" ).fadeOut( 500, function() {
+            $( ".modal-overlay" ).remove();
+        });
+    });
+
+    $( "#" + Mockup.pageId + " .save-tweet" ).unbind( "click" );
+    $( "#" + Mockup.pageId + " .save-tweet" ).bind( "click", function() {
+        var tweet = {
+            text: $( "#" + Mockup.pageId + " .create-tweet-text" ).val(),
+            date: new Date().getTime()
+        };
+        var activeUser = $( "#" + Mockup.pageId + " .resp-tab-active" ).attr( "user" );
+        var userName = Mockup.config.TIMELINES[ activeUser ].userName;
+        var tweetContent = Mockup.getTweetContent( activeUser, userName, tweet );
+        $( "#" + Mockup.pageId + " .resp-tab-content-active .timeline-tweetlist" ).prepend( tweetContent );
+        $( ".modal-box, .modal-overlay" ).fadeOut( 500, function() {
+            $( ".modal-overlay" ).remove();
+        });
     });
 };
 
@@ -24,7 +60,7 @@ Mockup.loadTimelines = function( config ) {
         var contentHtml = "";
         var listHtml = "";
 
-        listHtml += '<li>' + timeline + '</li>';
+        listHtml += '<li user=' + timeline + '>' + timeline + '</li>';
 
         contentHtml += '<div>';
         contentHtml += Mockup.generateTimelineContent( timeline, config ) + '</div>';
@@ -33,13 +69,15 @@ Mockup.loadTimelines = function( config ) {
         $( "#" + Mockup.pageId + " .container .resp-tabs-container" ).append( contentHtml );
     });
 
-    $( "#" + Mockup.pageId + " #feeds-tab" ).easyResponsiveTabs( {
+    $( "#" + Mockup.pageId + " #feeds-tab" ).easyResponsiveTabs({
         type: 'default',        
         width: 'auto',
         fit: true,
         closed: false,
         tabidentify: "feeds",
-        activate: function() { console.log( "Switch" ); }
+        activate: function() {
+
+        }
     });
 };
 
@@ -47,7 +85,7 @@ Mockup.generateTimelineContent = function( timeline, config ) {
     var timelineContent = "";
 
     timelineContent += "<div class=\"timeline-header\">" + Mockup.generateHeader( timeline ) + "</div>";
-    timelineContent += "<div class=\"timeline-body\">" + Mockup.generateContent( timeline, config ) + "</div>";
+    timelineContent += "<div class=\"timeline-body\">" + Mockup.generateContent( timeline, config.TIMELINES[ timeline ] ) + "</div>";
     timelineContent += "<footer class=\"timeline-footer\">" + Mockup.generateFooter( timeline ) + "</footer>";
 
     return timelineContent;
@@ -63,15 +101,32 @@ Mockup.generateHeader = function( timeline ) {
     return headerConent;
 };
 
-Mockup.generateContent = function( timeline, config ) {
+Mockup.generateContent = function( timeline, content ) {
     var bodyConent = "";
-    var tweets = config.TIMELINES[ timeline ].POSTS;
+    var userName = content.userName;
+    var tweets = content.POSTS;
     bodyConent += '<div class="timeline-viewport"><ol class="timeline-tweetlist">';
-    tweets && tweets.forEach( function() {
-        bodyConent += '<li class="timeline-tweetlist-tweet content-border">'
-        bodyConent += '</li>';
+    tweets && tweets.forEach( function( tweet ) {
+        bodyConent += Mockup.getTweetContent( timeline, userName, tweet );
     });
     bodyConent += "</ol></div>";
+
+    return bodyConent;
+};
+
+Mockup.getTweetContent = function( timeline, userName, tweet ) {
+    var bodyConent = "";
+    bodyConent += '<li class="timeline-tweetlist-tweet content-border">'
+    bodyConent +=   '<div class="timeline-tweet-author">'
+    bodyConent +=       '<span class="tweet-avatar">';
+    bodyConent +=           '<img>';
+    bodyConent +=       '</span>';
+    bodyConent +=       '<a href="https://twitter.com/' + timeline + '" class="tweet-link">';
+    bodyConent +=           '<span class="tweet-name">' + userName + '</span>';
+    bodyConent +=           '<span class="tweet-link"> @' + timeline + '</span>';
+    bodyConent +=       '</a>';
+    bodyConent +=   '</div>';
+    bodyConent += '</li>';
 
     return bodyConent;
 };
